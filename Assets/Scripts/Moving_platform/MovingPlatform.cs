6d89e8675796bb6171aa8ac963751ce0ac313f06
Keeper <gadzizulumhanov199@gmail.com>
2025-03-14 18:48:17 +0300

    Добавил движущиеся платформы

---
+++ Assets/Scripts/Moving_platform/MovingPlatform.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform pointA;
+    public Transform pointB;
+    public float speed = 2f;
+    private bool movingToB = true;
+
+    private void Update()
+    {
+        Transform target = movingToB ? pointB : pointA;
+        transform.position = Vector2.MoveTowards(transform.position, target.position, speed * Time.deltaTime);
+
+        if (Vector2.Distance(transform.position, target.position) < 0.1f)
+        {
+            movingToB = !movingToB;
+        }
+    }
+
+    // Когда игрок встал на платформу – делаем его дочерним объектом
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(transform);
+        }
+    }
+
+    // Когда игрок спрыгнул с платформы – убираем связь
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            collision.transform.SetParent(null);
+        }
+    }
+}
